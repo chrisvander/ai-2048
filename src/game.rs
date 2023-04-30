b02efcab732e4fb3742e5a88ac8929e9b333402e
Christian van der Loo <chris.vanderloo@yahoo.com>
2023-04-29 23:32:12 -0400

    implemented game with keyboard

---
+++ src/game.rs
@@ -0,0 +1,257 @@
+use rand::{seq::SliceRandom, thread_rng, Rng};
+
+pub trait Agent {
+    fn get_move(&mut self, game: &Game) -> Move;
+    fn log_messages(&self) -> &str;
+    fn make_move(&mut self, game: &mut Game) {
+        game.update(self.get_move(game));
+    }
+}
+
+pub struct Game {
+    state: [u16; 16],
+}
+
+impl Default for Game {
+    fn default() -> Self {
+        let mut g = Game { state: [0; 16] };
+        g.generate_tile();
+        g.generate_tile();
+        g
+    }
+}
+
+impl Clone for Game {
+    fn clone(&self) -> Self {
+        Game { state: self.state }
+    }
+}
+
+#[derive(PartialEq)]
+pub enum Move {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+fn merge_duplicates(v: Vec<u16>) -> Vec<u16> {
+    let mut r = vec![];
+    let mut skip = false;
+    for i in 0..v.len() {
+        if skip {
+            skip = false;
+            continue;
+        }
+        if i < v.len() - 1 && v[i] == v[i + 1] {
+            r.push(v[i] + 1);
+            skip = true;
+        } else {
+            r.push(v[i]);
+        }
+    }
+    r
+}
+
+impl Game {
+    pub fn new() -> Self {
+        Game::default()
+    }
+
+    pub fn update(&mut self, input: Move) {
+        let state_before = self.state.clone();
+        self.shift(input);
+        // element wise compare
+        if state_before != self.state {
+            self.generate_tile();
+        }
+    }
+
+    fn xy_to_index(x: u8, y: u8) -> usize {
+        (x + y * 4) as usize
+    }
+
+    pub fn get_tile(&self, x: u8, y: u8) -> u16 {
+        self.state[Game::xy_to_index(x, y)]
+    }
+
+    pub fn set_tile(&mut self, x: u8, y: u8, value: u16) {
+        self.state[Game::xy_to_index(x, y)] = value;
+    }
+
+    pub fn get_table(&self) -> Vec<Vec<u32>> {
+        let pows = (&self.state)
+            .iter()
+            .map(|n| 2_u32.pow(*n as u32))
+            .collect::<Vec<_>>();
+        pows.chunks(4).map(|s| s.into()).collect()
+    }
+
+    pub fn get_state(&self) -> &[u16; 16] {
+        &self.state
+    }
+
+    pub fn set_state(&mut self, s: [u16; 16]) {
+        self.state = s;
+    }
+
+    fn get_condensed_rows(&self) -> Vec<Vec<u16>> {
+        // get vec of rows with empty removed
+        self.state
+            .chunks(4)
+            .map(|row| {
+                row.iter()
+                    .map(|n| n.to_owned())
+                    .filter(|n| *n != 0)
+                    .collect()
+            })
+            .collect()
+    }
+
+    fn get_condensed_cols(&self) -> Vec<Vec<u16>> {
+        // get vec of cols with empty removed
+        self.state
+            .iter()
+            .enumerate()
+            .fold(vec![vec![]; 4], |mut acc, (i, n)| {
+                if *n != 0 {
+                    acc[i % 4].push(*n);
+                }
+                acc
+            })
+    }
+
+    fn shift(&mut self, input: Move) {
+        let condensed = match input {
+            Move::Up | Move::Down => self.get_condensed_cols(),
+            Move::Left | Move::Right => self.get_condensed_rows(),
+        };
+
+        let new_state: [u16; 16] = condensed
+            .iter()
+            .map(|v| merge_duplicates(v.to_owned()))
+            .map(|mut v| {
+                let mut r = vec![0; 4 - v.len()];
+                if input == Move::Up || input == Move::Left {
+                    v.append(&mut r);
+                    v
+                } else {
+                    r.append(&mut v);
+                    r
+                }
+            })
+            .flatten()
+            .collect::<Vec<_>>()
+            .try_into()
+            .unwrap();
+
+        match input {
+            Move::Up | Move::Down => {
+                let mut transposed = vec![0; 16];
+                transpose::transpose(&mut Vec::from(new_state), &mut transposed, 4, 4);
+                self.set_state(transposed.try_into().unwrap());
+            }
+            Move::Left | Move::Right => self.set_state(new_state),
+        }
+    }
+
+    fn generate_tile(&mut self) {
+        let n = thread_rng().gen_range(1..=2);
+
+        // get indexes of empty tiles
+        let empty_indexes = self
+            .state
+            .iter()
+            .enumerate()
+            .filter(|(_, &v)| v == 0)
+            .map(|(i, _)| i)
+            .collect::<Vec<_>>();
+
+        if empty_indexes.len() == 0 {
+            return;
+        }
+
+        let chosen_index = empty_indexes.choose(&mut thread_rng()).unwrap();
+        self.state[*chosen_index] = n;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn set_state_and_tile() {
+        let mut game = Game::default();
+        assert_eq!(game.get_tile(0, 0), 0);
+
+        game.set_state([
+            0x12, 0x34, 0x56, 0x78, 0x9a, 0xbc, 0xde, 0xf0, 0x12, 0x34, 0x56, 0x78, 0x9a, 0xbc,
+            0xde, 0xf0,
+        ]);
+
+        assert_eq!(game.get_tile(0, 0), 0x12);
+        assert_eq!(game.get_tile(1, 0), 0x34);
+        assert_eq!(game.get_tile(2, 0), 0x56);
+        assert_eq!(game.get_tile(3, 0), 0x78);
+        assert_eq!(game.get_tile(0, 1), 0x9a);
+
+        game.set_tile(0, 1, 0x00);
+        assert_eq!(game.get_tile(0, 1), 0x00);
+    }
+
+    #[test]
+    fn test_shift() {
+        let mut game = Game::default();
+
+        game.set_state([0; 16]);
+        game.set_tile(0, 0, 1);
+        game.set_tile(1, 0, 1);
+
+        game.shift(Move::Right);
+
+        assert_eq!(game.get_tile(0, 0), 0);
+        assert_eq!(game.get_tile(1, 0), 0);
+        assert_eq!(game.get_tile(2, 0), 0);
+        assert_eq!(game.get_tile(3, 0), 2);
+
+        game.shift(Move::Left);
+
+        assert_eq!(game.get_tile(0, 0), 2);
+
+        game.shift(Move::Up);
+
+        dbg!(game.get_state());
+
+        assert_eq!(game.get_tile(0, 3), 2);
+    }
+
+    #[test]
+    fn test_merge_duplicates() {
+        let v = vec![1, 1, 2, 2];
+        assert_eq!(merge_duplicates(v), vec![2, 3]);
+        let v = vec![1, 2, 2, 2];
+        assert_eq!(merge_duplicates(v), vec![1, 3, 2]);
+        let v = vec![1, 1, 1, 1];
+        assert_eq!(merge_duplicates(v), vec![2, 2]);
+        let v = vec![1, 2, 2, 6];
+        assert_eq!(merge_duplicates(v), vec![1, 3, 6]);
+    }
+
+    #[test]
+    fn test_condensed_getters() {
+        let mut game = Game::default();
+
+        game.set_state([0; 16]);
+        game.set_tile(0, 0, 1);
+        game.set_tile(1, 0, 1);
+        game.set_tile(0, 3, 2);
+
+        let condensed_rows = game.get_condensed_rows();
+        let condensed_cols = game.get_condensed_cols();
+
+        assert_eq!(condensed_rows[0], vec![1, 1]);
+        assert_eq!(condensed_rows[3], vec![2]);
+        assert_eq!(condensed_cols[0], vec![1, 2]);
+        assert_eq!(condensed_cols[1], vec![1]);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,238 @@
-use std::{io, thread, time::Duration};
+use crate::game::*;
+use crossterm::{
+    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
+use std::{error::Error, io, time::Duration};
 use tui::{
     backend::{Backend, CrosstermBackend},
-    layout::{Constraint, Direction, Layout},
-    widgets::{Block, Borders, Widget},
+    layout::{Constraint, Direction, Layout, Rect},
+    style::{Color, Modifier, Style},
+    text::{Span, Spans, Text},
+    widgets::{Block, Borders, Cell, List, ListItem, ListState, Paragraph, Row, Table, Wrap},
     Frame, Terminal,
 };
 
-struct Game {
-    state: u64,
+mod game;
+
+static TICK_RATE: Duration = Duration::from_millis(250);
+static MENU_ITEMS: &[&str] = &["Play (Keyboard)", "Play (Expectimax)", "Play (RL)"];
+
+pub enum Screen {
+    Menu {
+        state: ListState,
+        menu: List<'static>,
+    },
+    KeyboardGame(Game),
+    AIGame(Box<dyn Agent>, Game),
 }
 
-impl Game {
-    fn new() -> Self {
-        Game { state: 0 }
+pub struct App {
+    screen: Screen,
+}
+
+impl Default for Screen {
+    fn default() -> Self {
+        let mut state = ListState::default();
+        state.select(Some(0));
+
+        let menu = List::new(
+            MENU_ITEMS
+                .iter()
+                .map(|s| ListItem::new(*s))
+                .collect::<Vec<ListItem<'static>>>(),
+        );
+
+        Screen::Menu { state, menu }
     }
 }
 
-fn ui<B: Backend>(f: &mut Frame<B>) {
-    let chunks = Layout::default()
-        .direction(Direction::Horizontal)
-        .margin(1)
-        .constraints([Constraint::Min(0), Constraint::Length(40)].as_ref())
-        .split(f.size());
-    let block = Block::default().title("Game").borders(Borders::ALL);
-    f.render_widget(block, chunks[0]);
-    let block = Block::default().title("Menu").borders(Borders::ALL);
-    f.render_widget(block, chunks[1]);
+impl Default for App {
+    fn default() -> Self {
+        App {
+            screen: Screen::default(),
+        }
+    }
 }
 
-fn main() -> Result<(), io::Error> {
-    let stdout = io::stdout();
+fn render_game<B: Backend>(f: &mut Frame<B>, block: Block<'_>, game: &Game, rect: Rect) {
+    let game_state = game.get_table();
+
+    let selected_style = Style::default().add_modifier(Modifier::REVERSED);
+    let normal_style = Style::default().bg(Color::DarkGray);
+    let rows = game_state.iter().map(|row| {
+        let cells = row.iter().map(|c| {
+            let cstr = if *c == 1 {
+                String::from("")
+            } else {
+                c.to_string()
+            };
+
+            Cell::from(Text::from(
+                [
+                    "\n\n",
+                    " ".repeat(5 - (cstr.len() / 2)).as_str(),
+                    cstr.as_str(),
+                    "\n\n",
+                ]
+                .join(""),
+            ))
+            .style(normal_style)
+        });
+        Row::new(cells).height(5).bottom_margin(1)
+    });
+    let t = Table::new(rows)
+        .block(block)
+        .highlight_style(selected_style)
+        .highlight_symbol(">> ")
+        .widths(&[
+            Constraint::Length(11),
+            Constraint::Length(11),
+            Constraint::Length(11),
+            Constraint::Length(11),
+        ]);
+
+    f.render_widget(t, rect);
+}
+
+fn ui<B: Backend>(f: &mut Frame<B>, app: &mut App) {
+    let chunks = Layout::default()
+        .direction(Direction::Vertical)
+        .margin(1)
+        .constraints([Constraint::Min(0), Constraint::Length(20)].as_ref())
+        .split(f.size());
+
+    match &mut app.screen {
+        Screen::Menu { state, menu } => {
+            let block = Block::default().title("Menu").borders(Borders::ALL);
+            let list = menu
+                .clone()
+                .block(block)
+                .highlight_style(Style::default().fg(Color::Yellow))
+                .highlight_symbol(">> ");
+
+            f.render_stateful_widget(list, chunks[0], state);
+
+            let block = Block::default().title("Info").borders(Borders::ALL);
+            let paragraph = Paragraph::new("Use arrow keys to change menu items\nPress q to exit")
+                .block(block)
+                .wrap(Wrap { trim: true });
+            f.render_widget(paragraph, chunks[1]);
+        }
+        Screen::KeyboardGame(game) => {
+            let game_block = Block::default().title("Game").borders(Borders::ALL);
+            render_game(f, game_block, game, chunks[0]);
+            let block = Block::default().title("Info").borders(Borders::ALL);
+            let paragraph = Paragraph::new("Use arrow keys to move the tiles\nPress q to exit")
+                .block(block)
+                .wrap(Wrap { trim: true });
+
+            f.render_widget(paragraph, chunks[1]);
+        }
+        Screen::AIGame(agent, game) => {
+            let game_block = Block::default().title("Game").borders(Borders::ALL);
+            f.render_widget(game_block, chunks[0]);
+            let block = Block::default().title("Log").borders(Borders::ALL);
+            let paragraph = Paragraph::new(agent.log_messages())
+                .block(block)
+                .wrap(Wrap { trim: true });
+            f.render_widget(paragraph, chunks[1]);
+        }
+    }
+}
+
+fn run_app<B: Backend>(terminal: &mut Terminal<B>) -> io::Result<()> {
+    terminal.clear()?;
+    let mut app = App::default();
+    let mut last_tick = std::time::Instant::now();
+    loop {
+        terminal.draw(|f| ui(f, &mut app))?;
+
+        let timeout = TICK_RATE
+            .checked_sub(last_tick.elapsed())
+            .unwrap_or_else(|| Duration::from_secs(0));
+
+        match event::poll(timeout)? {
+            true => match &mut app.screen {
+                Screen::Menu { state, menu: _ } => {
+                    let event = event::read().unwrap();
+                    match event {
+                        Event::Key(event) => match event.code {
+                            KeyCode::Char('q') => break Ok(()),
+                            KeyCode::Up => {
+                                let Some(sel) = state.selected() else { continue };
+                                if sel > 0 {
+                                    state.select(Some(sel - 1));
+                                }
+                            }
+                            KeyCode::Down => {
+                                let Some(sel) = state.selected() else { continue };
+                                if sel < MENU_ITEMS.len() - 1 {
+                                    state.select(Some(sel + 1));
+                                }
+                            }
+                            KeyCode::Enter => match state.selected() {
+                                Some(0) => {
+                                    app.screen = Screen::KeyboardGame(Game::new());
+                                }
+                                _ => {}
+                            },
+                            _ => {}
+                        },
+                        _ => {}
+                    }
+                }
+                Screen::KeyboardGame(game) => {
+                    let Ok(keyboard_move) = (match event::read().unwrap() {
+                        Event::Key(event) => match event.code {
+                            KeyCode::Char('q') => { app.screen = Screen::default(); continue },
+                            KeyCode::Char('w') => Ok(Move::Up),
+                            KeyCode::Char('a') => Ok(Move::Left),
+                            KeyCode::Char('s') => Ok(Move::Down),
+                            KeyCode::Char('d') => Ok(Move::Right),
+                            KeyCode::Up => Ok(Move::Up),
+                            KeyCode::Left => Ok(Move::Left),
+                            KeyCode::Down => Ok(Move::Down),
+                            KeyCode::Right => Ok(Move::Right),
+                            _ => Err("Invalid key"),
+                        },
+                        _ => Err("Event not a key"),
+                    }) else {
+                        continue;
+                    };
+
+                    game.update(keyboard_move);
+                }
+                Screen::AIGame(agent, game) => {
+                    agent.make_move(game);
+                }
+            },
+            _ => {}
+        };
+
+        if last_tick.elapsed() >= TICK_RATE {
+            last_tick = std::time::Instant::now();
+        }
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    enable_raw_mode()?;
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
     let backend = CrosstermBackend::new(stdout);
     let mut terminal = Terminal::new(backend).unwrap();
-    terminal.clear()?;
-    terminal.draw(ui)?;
-    thread::sleep(Duration::from_millis(5000));
 
-    let game = Game::new();
+    run_app(&mut terminal)?;
+
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    terminal.show_cursor()?;
 
     Ok(())
 }

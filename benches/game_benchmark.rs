c0907ae693d4e1631db8669017094a985198b801
Christian van der Loo <chris.vanderloo@yahoo.com>
2023-07-09 14:35:49 -0400

    added some benchmarks

--- benches/game_benchmark.rs
+++ benches/game_benchmark.rs
@@ -1,4 +1,8 @@
-use ai_2048::{agent::random::RandomAgent, agent::Agent, game::Game};
+use ai_2048::{
+    agent::random::RandomAgent,
+    agent::Agent,
+    game::{Game, Move},
+};
 use criterion::{black_box, criterion_group, criterion_main, BatchSize, Criterion};
 
 fn criterion_benchmark(c: &mut Criterion) {
@@ -13,6 +17,48 @@ fn criterion_benchmark(c: &mut Criterion) {
                 while !agent.get_game().game_over() {
                     agent.next_move();
                 }
+            },
+            BatchSize::SmallInput,
+        )
+    });
+
+    c.bench_function("merge horizontal", |b| {
+        b.iter_batched(
+            || {
+                let mut game = black_box(Game::new_seeded(0));
+                // spin the board around a bit
+                for _ in 0..10 {
+                    game.update(Move::Up);
+                    game.update(Move::Left);
+                    game.update(Move::Right);
+                    game.update(Move::Down);
+                }
+                game
+            },
+            |mut game| {
+                game.update(black_box(Move::Left));
+                game.update(black_box(Move::Right));
+            },
+            BatchSize::SmallInput,
+        )
+    });
+
+    c.bench_function("merge vertical", |b| {
+        b.iter_batched(
+            || {
+                let mut game = black_box(Game::new_seeded(0));
+                // spin the board around a bit
+                for _ in 0..10 {
+                    game.update(Move::Up);
+                    game.update(Move::Left);
+                    game.update(Move::Right);
+                    game.update(Move::Down);
+                }
+                game
+            },
+            |mut game| {
+                game.update(black_box(Move::Up));
+                game.update(black_box(Move::Down));
             },
             BatchSize::SmallInput,
         )
